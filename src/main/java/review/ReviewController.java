0956a23ea92027c09a2de4cf49e2992c71ba2e4c
Pascal <you@example.com>
2016-11-18 13:40:34 -0500

    created findAll() method

--- src/main/java/review/ReviewController.java
+++ src/main/java/review/ReviewController.java
@@ -1,5 +1,32 @@
 package review;
 
+import java.util.Collection;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ReviewController {
+
+	@Resource
+	private ReviewRepository myRepository;
+
+	@RequestMapping("/review")
+	public String displayReview(@RequestParam("id") long id, Model model) {
+		Review review = myRepository.findById(id);
+		model.addAttribute("selectedReview", review);
+		return "review-view";
+	}
+	
+	@RequestMapping("/all")
+	public String displayAll(Model model){
+	Collection<Review> reviews = myRepository.findAll();
+	model.addAttribute("reviews", reviews);
+	return "all-reviews";
+	}
 
 }
