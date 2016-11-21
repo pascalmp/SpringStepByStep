69c45269b24b91555d5097e040ab5c3c19e9a71c
Pascal <you@example.com>
2016-11-21 09:59:29 -0500

    Updates

--- src/main/java/review/Application.java
+++ src/main/java/review/Application.java
@@ -1,7 +1,13 @@
 package review;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,6 +15,27 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    
+	@Resource
+	private ReviewRepository reviewRepository;
+
+	@Bean
+	public CommandLineRunner populateReviews() {
+		return new ReviewPopulatorRunner();
+	}
+
+	public class ReviewPopulatorRunner implements CommandLineRunner {
+		@Override
+		public void run(String... args) throws Exception {
+			/*
+			 * Since it's the first Review being created, the generated id for this Review will be 1 (specifically, 1L since it's long).
+			 */
+			reviewRepository.save(new Review("title", "author", "content", new Date()));
+		}
+	}
+    
+    
 }
 
 
--- src/main/java/review/Review.java
+++ src/main/java/review/Review.java
@@ -2,7 +2,17 @@ package review;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Review {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
 	
 	private long id;
 	private String title;
@@ -10,14 +20,18 @@ public class Review {
 	private String content;
 	private Date date;
 	
-	public Review(long id, String title, String author, String content, Date date){
+	public Review(String title, String author, String content, Date date){
 		
-		this.id = id;
 		this.title = title;
 		this.author = author;
 		this.content = content;
 		this.date = date;
 	}
+	
+	protected Review(){
+		
+	}
+	
 	
 	public long getId(){
 		return id;
